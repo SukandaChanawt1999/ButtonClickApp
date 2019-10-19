c059ec788c2365269d602f67bdb8fb576844f1e7
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-19 14:12:41 +0700

    16.1 findViewById and r Class

--- app/src/main/java/com/example/buttonclickapp/MainActivity.kt
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.kt
@@ -2,8 +2,16 @@ package com.example.buttonclickapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var userInput: EditText? = null
+    private var button: Button? = null
+    private var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
